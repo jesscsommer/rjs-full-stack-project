1c7f59fd09110308245a71adaef0406f05e238c4
rcblake <127946751+rcblake@users.noreply.github.com>
2023-06-26 11:08:06 -0700

    all columns and relationships

--- server/models/models.py
+++ server/models/models.py
@@ -17,6 +17,8 @@ class User(db.Model,SerializerMixin):
     username = db.Column(db.String, nullable=False,unique=True)
     name = db.Column(db.String)
     bio = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
     
     # relationships
     comments = db.relationship("Comment", back_populates="user")
@@ -38,28 +40,61 @@ class Follow(db.Model,SerializerMixin):
     __tablename__="follows"
     
     id = db.Column(db.Integer, primary_key=True)
-    follower_id = db.Column(db.Integer, ForeignKey=("user.id"))
-    being_followed_id = db.Column(db.Integer, ForeignKey=("user.id"))
+    follower_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    being_followed_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    created_at = db.Colum(db.DateTime, server_default=db.func.now())
+
     
     follower=db.relationship("User",back_populates="follows")
     being_followed=db.relationship("User",back_populates="followers")
     
-class Comment(db.Model,SerializerMixin):
-    __tablename__="comments"
+    serialize_rules=("-follower.follows","-being_follwed.followers")
     
-    id = db.Column(db.Integer, primary_key=True)
     
 class Post(db.Model,SerializerMixin):
     __tablename__="posts"
     
     id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    created_at = db.Colum(db.DateTime, server_default=db.func.now())
+    
+    user = db.relationship("User", back_populates="posts")
+    comments = db.relationship("Comment", back_populates="post")
+    post_likes = db.relatioship("PostLikes", back_populates="post")
+    
+    
+class Comment(db.Model,SerializerMixin):
+    __tablename__="comments"
+    
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Integer, nullable=False)
+    post_id = db.Column(db.Integer,db.ForeignKey("post.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    created_at = db.Colum(db.DateTime, server_default=db.func.now())
+
+    
+    user = db.relationship("User", back_populates="comments")
+    post = db.relationship("Post", back_populates="comments")
+    comment_likes = db.relationship("CommentLikes", back_populates="comment")
     
 class PostLike(db.Model,SerializerMixin):
     __tablename__="post_likes"
     
     id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    created_at = db.Colum(db.DateTime, server_default=db.func.now())
+    
+    post = db.relationship("Post", back_populates="post_likes")
+
     
 class CommentLike(db.Model,SerializerMixin):
-    __tablename__="comment_like"
+    __tablename__="comment_likes"
     
     id = db.Column(db.Integer, primary_key=True)
+    comment_id = db.Column(db.Integer, db.ForeignKey("comment.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    created_at = db.Colum(db.DateTime, server_default=db.func.now())
+    
+    comment = db.relationship("Comment", back_populates="comment_likes")
