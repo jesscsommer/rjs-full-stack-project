d7aa65c40d0a92b89ea4c89417923d530568f720
jesscsommer <129113445+jesscsommer@users.noreply.github.com>
2023-06-26 16:46:55 -0700

    Update user_image.py
    Capitalize model

--- server/models/user_image.py
+++ server/models/user_image.py
@@ -1,6 +1,6 @@
 from .__init__ import db
 
-class UserImage(db.model):
+class UserImage(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     img = db.Column(db.String, unique=True, nullable=False)
     name = db.Column(db.String, nullable=False)
