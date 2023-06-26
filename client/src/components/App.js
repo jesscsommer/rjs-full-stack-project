424748a1701c8ee08de0c45566269f8d3b9b3bee
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-06-26 10:25:44 -0700

    Add sign in form component and template

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import SignUpForm from "./SignUpForm";
+import SignInForm from "./SignInForm";
 
 const App = () => {
-  // Code goes here!
-  <SignUpForm />
+  return (
+    <>
+      <SignUpForm />
+      <SignInForm />
+    </>
+  )
 }
 
 export default App;
--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -33,7 +33,7 @@ const SignUpForm = () => {
             <Typography component="h1" variant="h5">
                 Sign up
             </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Box component="form" noValidate sx={{ mt: 3 }}>
                 <Grid container spacing={2}>
                 <Grid item xs={12} sm={6}>
                     <TextField
@@ -101,7 +101,6 @@ const SignUpForm = () => {
                 </Grid>
             </Box>
             </Box>
-            <Copyright sx={{ mt: 5 }} />
             </Container>
         </ThemeProvider>
     )
