ddf8b811bbbbdf5a5761e8a88f5e8aad46b6b791
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-06-27 13:31:25 -0700

    Add hybrid property and setter for password

--- server/models/user.py
+++ server/models/user.py
@@ -1,4 +1,6 @@
 from .__init__ import db
+from config import bcrypt
+from sqlalchemy.ext.hybrid import hybrid_property
 
 user_following = db.Table(
     "user_following",
@@ -11,6 +13,7 @@ class User(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, nullable=False,unique=True)
+    _password_hash = db.Column(db.String)
     name = db.Column(db.String)
     bio = db.Column(db.String)
     profile_pic = db.Column(db.String)
@@ -31,4 +34,18 @@ class User(db.Model):
         backref = "followers"
     )
     
+    def __repr__(self):
+        return f"User #{self.id}: {self.username}"
     
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError("Password hashes may not be viewed")
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+    
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, 
+                                            password.encode('utf-8'))
