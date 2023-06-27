75e5dca1aa86b6083aeeada1fec44d09461700b5
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-06-27 09:32:33 -0700

    Add formik & yup dependencies

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -1,13 +1,14 @@
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import HistoryEduIcon from '@mui/icons-material/HistoryEdu';
-
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+
+import { useFormik } from "formik";
+import * as yup from "yup"
 
 const defaultTheme = createTheme();
 
