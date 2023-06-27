194dc5b6cf8939b78cec031a706be4db1c92e4bc
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-06-27 11:49:40 -0700

    Add separate inputs by line and logic to join in POST

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -10,7 +10,8 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 
 import { useFormik } from "formik";
-import * as yup from "yup"
+import * as yup from "yup";
+import { syllable } from "syllable";
 
 const defaultTheme = createTheme();
 
@@ -18,11 +19,29 @@ const PostForm = () => {
     const [errors, setErrors] = useState([])
 
     const postSchema = yup.object().shape({
-        content: yup.string()
+        line1: yup.string()
+        .test("is-5-syallables", "Line 1 must be 5 syllables", (value) => {
+            return syllable(value) == 5
+        })
+        .required("Line 1 is required"), 
+        line2: yup.string()
+        .test("is-7-syllables", "Line 2 must be 7 syllables", (value) => {
+            return syllable(value) == 7
+        })
+        .required("Line 2 is required"), 
+        line3: yup.string()
+        .test("is-5-syallables", "Line 3 must be 5 syllables", (value) => {
+            return syllable(value) == 5
+        })
+        .required("Line 3 is required")
     })
 
     const formik = useFormik({
-        initialValues: {}, 
+        initialValues: {
+            line1: "",
+            line2: "",
+            line3: ""
+        }, 
         validationSchema: postSchema, 
         onSubmit: (values) => {
             fetch("/posts", {
@@ -30,7 +49,9 @@ const PostForm = () => {
                 headers: {
                     "Content-Type": "application/json"
                 },
-                body: JSON.stringify(values)
+                body: JSON.stringify({
+                    "content": ([values.line1, values.line2, values.line3]).join(" \n")
+                })
             })
             .then(res => {
                 if (res.ok) {
@@ -49,6 +70,7 @@ const PostForm = () => {
                 <CssBaseline />
             <Box
             component="form"
+            onSubmit={formik.handleSubmit}
             sx={{
                 marginTop: 5,
                 display: 'flex',
@@ -61,27 +83,39 @@ const PostForm = () => {
                         <TextField
                         required
                         fullWidth
-                        id="line-1"
+                        id="line1"
+                        name="line1"
                         placeholder="Line 1"
+                        value={formik.values.line1}
                         variant="standard"
                         margin="normal"
+                        onChange={formik.handleChange}
                         />
+                        <p style={{ color: "red" }}>{formik.errors.line1}</p>
                         <TextField
                         required
                         fullWidth
-                        id="line-2"
+                        id="line2"
+                        name="line2"
                         placeholder="Line 2"
+                        value={formik.values.line2}
                         variant="standard"
                         margin="normal"
+                        onChange={formik.handleChange}
                         />
+                        <p style={{ color: "red" }}>{formik.errors.line2}</p>
                         <TextField
                         required
                         fullWidth
-                        id="line-3"
+                        id="line3"
+                        name="line3"
                         placeholder="Line 3"
+                        value={formik.values.line3}
                         variant="standard"
                         margin="normal"
+                        onChange={formik.handleChange}
                         />
+                        <p style={{ color: "red" }}>{formik.errors.line3}</p>
                         <Button 
                             type="submit"
                             fullWidth
