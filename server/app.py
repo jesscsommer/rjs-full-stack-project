9a5611b1567c25fa44adbc1405625a39558ee1d3
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-06-27 16:58:32 -0700

    Add CheckSession resource

--- server/app.py
+++ server/app.py
@@ -28,6 +28,7 @@ from blueprints.user_by_id import UserById
 from blueprints.signup import Signup
 from blueprints.login import Login
 from blueprints.logout import Logout
+from blueprints.check_session import CheckSession
 
 api.add_resource(CommentLikes, "/comment_likes")
 api.add_resource(CommentLikeById, "/comment_likes/<int:id>")
@@ -42,6 +43,7 @@ api.add_resource(UserById, "/users/<int:id>")
 api.add_resource(Signup, "/signup")
 api.add_resource(Login, "/login")
 api.add_resource(Logout, "/logout")
+api.add_resource(CheckSession, "/check_session")
 
 # This will go in the post/patch routes after the form has been created
 # saving file name for db
