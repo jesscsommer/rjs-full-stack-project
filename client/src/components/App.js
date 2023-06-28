df944f461dfbe3af7c567d07a0e716d17839605a
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-06-28 11:58:27 -0700

    Add back check_session useEffect

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,8 +9,17 @@ import RightSideBar from "./RightSideBar";
 import HeaderBar from "./HeaderBar";
 
 const App = () => {
-    const [currentUser, setCurrentUser] = useState([]);
+    const [currentUser, setCurrentUser] = useState(null);
     const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        fetch("/check_session").then((res) => {
+            if (res.ok) {
+                res.json().then(setCurrentUser);
+            }
+        });
+    }, []);
+    
 
     useEffect(() => {
         fetch("/posts")
@@ -19,26 +28,25 @@ const App = () => {
         .catch((err) => console.error(err));
     }, []);
 
-  return (
-    <div className="app">
-      <HeaderBar currentUser={currentUser} />
-      <Routes>
-        <Route path="/signup" element={<SignUpForm />} />
-        <Route path="/login" element={<LogInForm />} />
-        <Route path="/profile/:username" element={
-                <Profile 
-                    currentUser={currentUser} />
-                } />
-        <Route path="/" element={
-                <PostsContainer 
-                    currentUser={currentUser} 
-                    posts={posts} />
-                } />
-        />
-      </Routes>
-      {/* <RightSideBar /> */}
-    </div>
-  );
+    return (
+        <div className="app">
+        <HeaderBar currentUser={currentUser} />
+        <Routes>
+            <Route path="/signup" element={<SignUpForm />} />
+            <Route path="/login" element={<LogInForm />} />
+            <Route path="/profile/:username" element={
+                    <Profile 
+                        currentUser={currentUser} />
+                    } />
+            <Route path="/" element={
+                    <PostsContainer 
+                        currentUser={currentUser} 
+                        posts={posts} />
+                    } />
+        </Routes>
+        {/* <RightSideBar /> */}
+        </div>
+    );
 };
 
 
