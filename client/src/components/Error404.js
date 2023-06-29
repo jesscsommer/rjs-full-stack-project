e9f01ea9a2e368afffba54198fb47d4783a08b60
rcblake <127946751+rcblake@users.noreply.github.com>
2023-06-29 14:20:39 -0700

    meh 404 done

---
+++ client/src/components/Error404.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import { green, lightBlue } from "@mui/material/colors";
+import { useNavigate } from "react-router-dom";
+
+export default function Error404() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        flexDirection: "Column",
+      }}
+    >
+      <Typography variant="h1" style={{ color: "blue", marginTop: "-100px" }}>
+        404
+      </Typography>
+      <Typography variant="h5" sx={{ color: "green" }}>
+        <p>Page not found, dear friend,</p>
+        <p>Lost in the vast digital void,</p>
+        <p>Haiku whispers fade.</p>
+      </Typography>
+    </Box>
+  );
+}
