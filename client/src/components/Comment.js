7e912876742e18aa95f7014cd17367bce162d44f
rcblake <127946751+rcblake@users.noreply.github.com>
2023-06-29 11:24:05 -0700

    Add comment now function

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 import CardContent from "@mui/material/CardContent";
 import CardHeader from "@mui/material/CardHeader";
 import Typography from "@mui/material/Typography";
@@ -6,50 +6,51 @@ import IconButton from "@mui/material/IconButton";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 
-function Comment({currentUser, comment}){
+function Comment({ currentUser, comment }) {
+  const [likedComment, setLikedComment] = useState(false);
+  const [newLikedComment, setNewLikedComment] = useState([]);
 
-    const [likedComment, setLikedComment] = useState(false)
-    const [newLikedComment, setNewLikedComment] = useState([])
-
-    const handleLikedComment = () => {
-      if (currentUser){
-        setLikedComment(current => !current)
-        handleLikedCommentData()
-      } else {
-        alert('Please login first!')
-      }
+  const handleLikedComment = () => {
+    if (currentUser) {
+      setLikedComment((current) => !current);
+      handleLikedCommentData();
+    } else {
+      alert("Please login first!");
     }
+  };
 
-    const handleLikedCommentData = () => {
-        if (likedComment) {
-          fetch(`/comment_likes/${newLikedComment.id}`,{
-            method: 'DELETE'
-          })
-        } else {
-          fetch("/comment_likes", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({comment_id: comment.id, user_id: currentUser.id}),
-          })
-            .then((res) => res.json())
-            .then((like) => setNewLikedComment(like))
-            .catch((err) => console.error(err));
-        }
+  const handleLikedCommentData = () => {
+    if (likedComment) {
+      fetch(`/comment_likes/${newLikedComment.id}`, {
+        method: "DELETE",
+      });
+    } else {
+      fetch("/comment_likes", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          comment_id: comment.id,
+          user_id: currentUser.id,
+        }),
+      })
+        .then((res) => res.json())
+        .then((like) => setNewLikedComment(like))
+        .catch((err) => console.error(err));
     }
-    return (
-        <div className="comment">
-            <CardHeader
-                title={comment.user.username}
-            />
-            <CardContent>
-                <Typography variant="body2" color="text.secondary">
-                {comment.content}
-                </Typography>
-            </CardContent>
-            <IconButton onClick={handleLikedComment}>
-                {likedComment ? <FavoriteIcon /> : <FavoriteBorderIcon />}
-            </IconButton>
-        </div>)
+  };
+  return (
+    <div className="comment">
+      <CardHeader title={comment.user.username} />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {comment.content}
+        </Typography>
+      </CardContent>
+      <IconButton onClick={handleLikedComment}>
+        {likedComment ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+      </IconButton>
+    </div>
+  );
 }
 
-export default Comment
+export default Comment;
--- server/blueprints/comments.py
+++ server/blueprints/comments.py
@@ -1,12 +1,41 @@
-from blueprints import Resource, Blueprint, make_response, g, abort 
+from blueprints import session, request, Resource, Blueprint, make_response, g, abort
 from models import db
 from models.comment import Comment
+from models.user import User
 from schemas.comment_schema import CommentSchema
 
+comment_schema = CommentSchema()
 comments_schema = CommentSchema(many=True)
 comments_bp = Blueprint("comments", __name__, url_prefix="/comments")
+
 
 class Comments(Resource):
     def get(self):
         comments = comments_schema.dump(Comment.query.all())
         return make_response(comments, 200)
+
+    def post(self):
+        try:
+            data = request.get_json()
+            content = data.get("content")
+            post_id = data.get("post_id")
+
+            if id := session.get("user_id"):
+                current_user = db.session.get(User, id)
+
+                new_comment = Comment(content=content)
+                new_comment.user = current_user
+                new_comment.post_id = post_id
+
+                comment_schema.validate(new_comment)
+
+                db.session.add(new_comment)
+                db.session.commit()
+                # import ipdb
+
+                # ipdb.set_trace()
+
+                return make_response(comment_schema.dump(new_comment), 201)
+        except Exception as e:
+            db.session.rollback()
+            return make_response({"errors": [str(e)]}, 400)
